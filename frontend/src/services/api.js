d252be70fe79c1af836be1bd17c4230fb5f0489b
Hamrutha <hamruthasundar247@gmail.com>
2026-06-10 22:15:02 +0530

    Connect frontend to Render backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-baseURL: "http://localhost:5000"
+  baseURL: "https://smartstock-api-qrr5.onrender.com"
 });
 
 export const getDashboard = () =>
